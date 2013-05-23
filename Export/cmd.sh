61542831ddaff983bc1f80e3f7c3d5e0c983491e
zmLiu <470301594@qq.com>
2013-05-23 17:54:41 +0800

    无

--- Export/cmd.sh
+++ Export/cmd.sh
@@ -1,4 +1,4 @@
-cp -rf AneSDK.swc AneSDK.tar
+cp -rf AneSDK_ActionScript.swc AneSDK.tar
 
 tar xvf AneSDK.tar
 
@@ -8,9 +8,9 @@ cp library.swf iPhone-ARM/library.swf
 
 cp library.swf Default/library.swf
 
-/Applications/Adobe\ Flash\ Builder\ 4.7/sdks/4.6.0/bin/adt -package -target ane AneSDK_Android.ane extension_android.xml -swc AneSDK.swc -platform Android-ARM -C Android-ARM . -platform default -C Default .
+/Applications/Adobe\ Flash\ Builder\ 4.7/sdks/4.6.0/bin/adt -package -target ane AneSDK_Android.ane extension_android.xml -swc AneSDK_ActionScript.swc -platform Android-ARM -C Android-ARM . -platform default -C Default .
 
-/Applications/Adobe\ Flash\ Builder\ 4.7/sdks/4.6.0/bin/adt -package -target ane AneSDK_IOS.ane extension_ios.xml -swc AneSDK.swc -platform iPhone-ARM -C iPhone-ARM . -platformoptions platform.xml -platform default -C default .
+/Applications/Adobe\ Flash\ Builder\ 4.7/sdks/4.6.0/bin/adt -package -target ane AneSDK_IOS.ane extension_ios.xml -swc AneSDK_ActionScript.swc -platform iPhone-ARM -C iPhone-ARM . -platformoptions platform.xml -platform default -C default .
 
 rm -rf AneSDK.tar
 rm -rf catalog.xml
